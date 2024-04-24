89ee7dab703dd12ddb18538fdff3d39d353d13e9
kosekijsx <leandrobordondev@gmail.com>
2024-04-24 16:19:22 -0300

    👷🏼‍♀️ Fcdn render fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import "./themes/css/colors.css";
 import Sedes from "./components/Sedes/index";
 export default function App() {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-  const googleKey = import.meta.env.VITE_GOOGLEKEY;
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${googleKey}&loading=async&libraries=places,marker`;
-    script.defer = true;
-    script.onload = () => setScriptLoaded(true);
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-  return <div className="bg-[#f5f5f5]">{scriptLoaded && <Sedes />}</div>;
+  return (
+    <div className="bg-[#f5f5f5]">
+      <Sedes />
+    </div>
+  );
 }
