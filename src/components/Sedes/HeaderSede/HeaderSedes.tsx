9f3f959475b4f3f4dcedbddfab218994a9960cf0
Leandro Bordon <leandro.bordon@balloon-group.com>
2024-10-01 14:54:31 -0300

    :sparkles: first commit fase 3

--- src/components/Sedes/HeaderSede/HeaderSedes.tsx
+++ src/components/Sedes/HeaderSede/HeaderSedes.tsx
@@ -3,9 +3,9 @@ import { useFetch } from "../../../hooks/useFetch";
 import { StateSedes } from "../../../context/closestBranchStore";
 
 export default function HeaderSedes() {
-  const { data } = useFetch<StateSedes>(
-    "https://bgwp.bgroup.com.ar/wp-json/wp/v2/pages/63"
-  );
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+  const initUrl = import.meta.env.VITE_INIT_URL;
+  const { data } = useFetch<StateSedes>(`${apiUrl}${initUrl}`);
   return (
     <div className="br-24 md:bg-white mb-3 transition-all duration-300 ease-in-out">
       <div className="p-4">
